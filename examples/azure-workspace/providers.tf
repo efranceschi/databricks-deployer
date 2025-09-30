babce180d2dbd7ccb074313230ac38f93433d94e
Eduardo Franceschi <eduardo.franceschi@databricks.com>
2025-09-29 23:08:33 -0300

    Joined versions.tf with providers.tf

--- examples/azure-workspace/providers.tf
+++ examples/azure-workspace/providers.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = ">= 1.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = var.azure_subscription_id
